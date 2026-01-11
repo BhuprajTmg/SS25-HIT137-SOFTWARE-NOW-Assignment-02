89a5ae33376b31355a40b6dcd55113af9e94240b
Bhupraj Tamang <bhupraj875@gmail.com>
2026-01-11 11:18:49 +0930

    feature: Create project folder structure

---
+++ question_two/config.py
@@ -0,0 +1,13 @@
+"""
+Configuration file for Temperature Analysis Program
+Contains all constants and settings
+"""
+
+# Folder paths
+TEMPERATURES_FOLDER = "temperatures"
+OUTPUT_FOLDER = "output"
+
+# Output file names
+OUTPUT_SEASONAL_AVG = "output/average_temp.txt"
+OUTPUT_TEMP_RANGE = "output/largest_temp_range_station.txt"
+OUTPUT_TEMP_STABILITY = "output/temperature_stability_stations.txt"
