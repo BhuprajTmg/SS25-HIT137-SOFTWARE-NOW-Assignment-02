395dfd01a704cfa7a616623b519f78f10af38e2c
Kumosh <119304875+Kumosh@users.noreply.github.com>
2026-01-11 12:06:29 +0930

    Delete verification.py
    unessary file got pull

--- verification.py
+++
@@ -1,5 +0,0 @@
-def verify(original_text, decrypted_text):
-    if original_text == decrypted_text:
-        print("Decryption successful! Text matches original.")
-    else:
-        print("Decryption failed! Text does not match original.")
