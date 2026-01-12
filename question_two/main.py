e2e614b613105ad8ccbe6a514b406d5d37c5c254
Bhupraj Tamang <bhupraj875@gmail.com>
2026-01-12 21:47:50 +0930

    feature: Create main.py with imports and header

--- question_two/main.py
+++ question_two/main.py
@@ -1,1 +1,23 @@
-print("This is main file.")
+from load_data import load_all_temperature_data
+from transform_data import transform_to_long_format
+from analyze_seasonal import calculate_seasonal_averages
+from analyze_range import find_largest_range_stations
+from analyze_stability import analyze_temperature_stability
+from save_results import (
+    save_seasonal_averages,
+    save_range_results,
+    save_stability_results
+)
+from config import (
+    OUTPUT_SEASONAL_AVG,
+    OUTPUT_TEMP_RANGE,
+    OUTPUT_TEMP_STABILITY
+)
+
+
+def print_header():
+    """Print program header."""
+    print("=" * 70)
+    print(" " * 18 + "Temperature Analysis Program")
+    print("=" * 70)
+    print()
